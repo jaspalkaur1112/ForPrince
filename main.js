808508488f70c28baf38f4bcdeb02718b947fae9
jaspalkaur1112 <74783690+jaspalkaur1112@users.noreply.github.com>
2023-01-01 16:37:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia(
+    { 
+      audio: true
+    });
+
+classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/kA4F-80yW/model.json', modelReady);
+}
+
+function modelReady(){
+  classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+
+  }
+}
